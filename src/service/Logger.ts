0156e9be21c215b2eddf3afd9425ff1a7c76457d
Gadiel Rangel <machadogadiel@icloud.com>
2024-07-05 11:56:45 -0300

    feat: make logs readable

--- src/service/Logger.ts
+++ src/service/Logger.ts
@@ -3,16 +3,8 @@ import winston from "winston";
 
 const logger = winston.createLogger({
   level: "info",
-  format: winston.format.json(),
-  defaultMeta: { service: "user-service" },
-  transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    new winston.transports.File({ filename: "log/error.log", level: "error" }),
-    new winston.transports.File({ filename: "log/combined.log" }),
-  ],
+  format: winston.format.cli(),
+  transports: [new winston.transports.Console()],
 });
 
 //
