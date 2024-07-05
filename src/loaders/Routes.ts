38c34ba84d4bf85873ad1213021bc6742d727038
Gadiel Rangel <machadogadiel@icloud.com>
2024-07-05 06:49:50 -0300

    style: format RoutesLoader file

--- src/loaders/Routes.ts
+++ src/loaders/Routes.ts
@@ -1,39 +1,42 @@
 import { Express, Router } from "express";
 
-import glob from 'fast-glob';
-import fs from 'fs';
-import path from 'path';
+import glob from "fast-glob";
+import fs from "fs";
+import path from "path";
 import logger from "../service/Logger";
 
-const BASE_DIR = path.join(__dirname, '../../');
+const BASE_DIR = path.join(__dirname, "../../");
 
 export default class RoutesLoader {
   async fromFiles(globPattern: string): Promise<Router> {
     let router = Router();
     let files: string[] = [];
-    
+
     try {
       files = await glob(globPattern, { cwd: BASE_DIR });
     } catch (error) {
       logger.error(error);
     }
-  
+
     for (const file of files) {
-      if (fs.statSync(file).isFile() && path.extname(file).toLowerCase() === '.ts') {
+      if (
+        fs.statSync(file).isFile() &&
+        path.extname(file).toLowerCase() === ".ts"
+      ) {
         try {
           const routeModule = await import(path.resolve(file));
 
           router = (routeModule.default || routeModule)(router);
         } catch (error) {
-          logger.error(error)
+          logger.error(error);
         }
       }
     }
-  
+
     return router;
   }
 
   mount(app: Express, router: Router) {
-    app.use('/', router)
+    app.use("/", router);
   }
 }
